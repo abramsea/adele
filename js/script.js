d6a7a2975bc7db2e8becc959443eeb3a38afa457
Evgenii Abramson <48097553+abramsea@users.noreply.github.com>
2019-07-23 01:20:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-menu.onclick = function openHiddenMenu() {
-    var x = document.getElementById('myTopnav');
-
-    if (x.className === "topnav") {
-        x.className += " responsive" ;
-    } else {
-        x.className = "topnav";
-    }
-
-}
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+      
